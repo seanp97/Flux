e8ff1feedd278a6a2d0cd2cd47307edd4c61546d
Sean Pelser <sean.pelser97@gmail.com>
2024-04-21 20:44:04 +0100

    base heade footer

--- base/header.php
+++ base/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= base_url(); ?>/css/style.css">
-    <script defer src="<?= base_url(); ?>/js/main.js"></script>
+    <link rel="stylesheet" href="<?= BaseURL(); ?>/css/style.css">
+    <script defer src="<?= BaseURL(); ?>/js/main.js"></script>
 </head>
 <body>
