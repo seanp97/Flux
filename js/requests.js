056387d6cd0a7c93226c19c94fb8a50a248b7cbe
Sean Pelser <sean.pelser97@gmail.com>
2024-04-22 16:15:54 +0100

    Requests update

--- js/requests.js
+++ js/requests.js
@@ -1,15 +1,18 @@
-const GetJSON = async (url) => {
-    const _data = await fetch(url);
-    const _json = await _data.json();
-    return _json;
+class HttpRequest {
+    
+    GetJSON = async (url) => {
+        this._data = await fetch(url);
+        this._json = await this._data.json();
+        return this._json;
+    }
+    
+    PostJSON = async (url, data) => {
+        fetch(url, {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'}, 
+            body: JSON.stringify(data)
+          }).then(res => {
+            console.log(res);
+        });
+    };
 }
-
-const PostJSON = async (url, data) => {
-    fetch(url, {
-        method: "POST",
-        headers: {'Content-Type': 'application/json'}, 
-        body: JSON.stringify(data)
-      }).then(res => {
-        console.log(res);
-    });
-};
