cd308dffd0ab7a56714d88a207d81fdd92b8330b
Sean Pelser <sean.pelser97@gmail.com>
2024-04-22 16:07:53 +0100

    Update header.php

--- base/header.php
+++ base/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= BaseURL(); ?>/css/style.css">
+    <script defer src="<?= BaseURL(); ?>/js/requests.js"></script>
     <script defer src="<?= BaseURL(); ?>/js/main.js"></script>
 </head>
 <body>
