e0cd8bbb7d4feb87387be15a3155602cab51ec41
Sean Pelser <sean.pelser97@gmail.com>
2024-10-08 22:23:21 +0100

    Update flux-model.php - try/catch blocks added to methods

--- core/flux-model.php
+++ core/flux-model.php
@@ -77,85 +77,107 @@ class FluxModel {
     }
 
     public static function Find($value) {
-        $callingClass = get_called_class();
-        self::$modelQueryBuilder = "SELECT * FROM $callingClass WHERE ";
-        $properties = get_class_vars($callingClass);
-        $first = true;
-        foreach ($properties as $propertyName => $propertyValue) {
-            if ($propertyName === 'modelQueryBuilder' || $propertyName === 'modelQueryParams') {
-                continue;
+        try {
+            $callingClass = get_called_class();
+            self::$modelQueryBuilder = "SELECT * FROM $callingClass WHERE ";
+            $properties = get_class_vars($callingClass);
+            $first = true;
+            foreach ($properties as $propertyName => $propertyValue) {
+                if ($propertyName === 'modelQueryBuilder' || $propertyName === 'modelQueryParams') {
+                    continue;
+                }
+                if (!$first) {
+                    self::$modelQueryBuilder .= " OR ";
+                } else {
+                    $first = false;
+                }
+                self::$modelQueryBuilder .= "$propertyName = $value";
+                self::$modelQueryParams[] = $value;
             }
-            if (!$first) {
-                self::$modelQueryBuilder .= " OR ";
-            } else {
-                $first = false;
-            }
-            self::$modelQueryBuilder .= "$propertyName = $value";
-            self::$modelQueryParams[] = $value;
+            return new self();
         }
-        return new self();
+        catch(Exception $e) {
+            throw new Exception('Find failed: ' . $e->getMessage());
+        }
     }   
 
     public static function FindOne($value) {
-        $callingClass = get_called_class();
-        self::$modelQueryBuilder = "SELECT * FROM $callingClass WHERE ";
-        $properties = get_class_vars($callingClass);
-        $first = true;
-        foreach ($properties as $propertyName => $propertyValue) {
-            if ($propertyName === 'modelQueryBuilder' || $propertyName === 'modelQueryParams') {
-                continue;
+        try {
+            $callingClass = get_called_class();
+            self::$modelQueryBuilder = "SELECT * FROM $callingClass WHERE ";
+            $properties = get_class_vars($callingClass);
+            $first = true;
+            foreach ($properties as $propertyName => $propertyValue) {
+                if ($propertyName === 'modelQueryBuilder' || $propertyName === 'modelQueryParams') {
+                    continue;
+                }
+                if (!$first) {
+                    self::$modelQueryBuilder .= " OR ";
+                } else {
+                    $first = false;
+                }
+                self::$modelQueryBuilder .= "$propertyName = $value";
+                self::$modelQueryParams[] = $value;
             }
-            if (!$first) {
-                self::$modelQueryBuilder .= " OR ";
-            } else {
-                $first = false;
-            }
-            self::$modelQueryBuilder .= "$propertyName = $value";
-            self::$modelQueryParams[] = $value;
+    
+            self::$modelQueryBuilder .= " LIMIT 1";
+            return new self();
         }
 
-        self::$modelQueryBuilder .= " LIMIT 1";
-        return new self();
+        catch(Exception $e) {
+            throw new Exception('FindOne failed: ' . $e->getMessage());
+        }
+
     } 
 
     public static function FirstProperty($className) {
-        $reflectionClass = new ReflectionClass($className);
-        $properties = $reflectionClass->getProperties(ReflectionProperty::IS_PUBLIC);
-
-        foreach ($properties as $property) {
-            $propertyType = $property->getType();
-            if ($propertyType && ($propertyType->getName() === 'int' || $propertyType->getName() === 'integer')) {
-                return $property->getName();
+        try {
+            $reflectionClass = new ReflectionClass($className);
+            $properties = $reflectionClass->getProperties(ReflectionProperty::IS_PUBLIC);
+    
+            foreach ($properties as $property) {
+                $propertyType = $property->getType();
+                if ($propertyType && ($propertyType->getName() === 'int' || $propertyType->getName() === 'integer')) {
+                    return $property->getName();
+                }
             }
+            return null;
         }
-        return null;
+        catch(Exception $e) {
+            throw new Exception($e->getMessage());
+        }
     }
 
     private static function GetColumnType($phpType) {
-        switch ($phpType) {
-            case 'int':
-            case 'integer':
-                return 'INT';
-            case 'float':
-            case 'double':
-                return 'FLOAT';
-            case 'bool':
-            case 'boolean':
-                return 'BOOLEAN';
-            case 'string':
-                return 'TEXT(65535)';
-            case 'datetime':
-                return 'DATETIME';
-            case 'date':
-                return 'DATE';
-            case 'time':
-                return 'TIME';
-            case 'text':
-                return 'TEXT';
-            case 'json':
-                return 'JSON';
-            default:
-                return 'TEXT(65535)';
+        try {
+            switch ($phpType) {
+                case 'int':
+                case 'integer':
+                    return 'INT';
+                case 'float':
+                case 'double':
+                    return 'FLOAT';
+                case 'bool':
+                case 'boolean':
+                    return 'BOOLEAN';
+                case 'string':
+                    return 'TEXT(65535)';
+                case 'datetime':
+                    return 'DATETIME';
+                case 'date':
+                    return 'DATE';
+                case 'time':
+                    return 'TIME';
+                case 'text':
+                    return 'TEXT';
+                case 'json':
+                    return 'JSON';
+                default:
+                    return 'TEXT(65535)';
+            }
+        }
+        catch(Exception $e) {
+            throw new Exception($e->getMessage());
         }
     }
 
@@ -192,7 +214,7 @@ class FluxModel {
                 $propertyType = 'TEXT(65530)';
             
                 if (self::FirstProperty($className) && $propertyName === self::FirstProperty($className) && $propertyType != 'INT') {
-                    $idColumnName = $className;
+                    $idColumnName = $className . 'Id';
                     $sqlCreate .= "$idColumnName INT AUTO_INCREMENT PRIMARY KEY ";
                 }
             
@@ -243,111 +265,211 @@ class FluxModel {
     }
 
     public static function Count() {
-        $callingClass = get_called_class();
-        self::$modelQueryBuilder .= "SELECT COUNT(*) FROM $callingClass";
-        return new self();
+        try {
+            $callingClass = get_called_class();
+            self::$modelQueryBuilder .= "SELECT COUNT(*) FROM $callingClass";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("Count Exception: " . $e->getMessage());
+        }
     }
 
     public static function Max($field) {
-        $callingClass = get_called_class();
-        self::$modelQueryBuilder .= "SELECT MAX($field) FROM $callingClass";
-        return new self();
+        try {
+            $callingClass = get_called_class();
+            self::$modelQueryBuilder .= "SELECT MAX($field) FROM $callingClass";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("Max Exception: " . $e->getMessage());
+        }
     }
 
     public static function Min($field) {
-        $callingClass = get_called_class();
-        self::$modelQueryBuilder .= "SELECT MIN($field) FROM $callingClass";
-        return new self();
+        try {
+            $callingClass = get_called_class();
+            self::$modelQueryBuilder .= "SELECT MIN($field) FROM $callingClass";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("Min Exception: " . $e->getMessage());
+        }
     }
 
     public static function Sum($field) {
-        $callingClass = get_called_class();
-        self::$modelQueryBuilder .= "SELECT SUM($field) FROM $callingClass";
-        return new self();
+        try {
+            $callingClass = get_called_class();
+            self::$modelQueryBuilder .= "SELECT SUM($field) FROM $callingClass";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("Sum Exception: " . $e->getMessage());
+        }
     }
 
     public static function Avg($field) {
-        $callingClass = get_called_class();
-        self::$modelQueryBuilder .= "SELECT AVG($field) FROM $callingClass";
-        return new self();
+        try {
+            $callingClass = get_called_class();
+            self::$modelQueryBuilder .= "SELECT AVG($field) FROM $callingClass";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("Avg Exception: " . $e->getMessage());
+        }
     }
 
     public static function Like($q) {
-        self::$modelQueryBuilder .= " LIKE '%$q%'";
-        return new self();
+        try {
+            self::$modelQueryBuilder .= " LIKE '%$q%'";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("Like Exception: " . $e->getMessage());
+        }
     }
     
     public static function All() {
-        $callingClass = get_called_class();
-        self::$modelQueryBuilder .= "SELECT * FROM $callingClass";
-        return new self();
+        try {
+            $callingClass = get_called_class();
+            self::$modelQueryBuilder .= "SELECT * FROM $callingClass";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("All Exception: " . $e->getMessage());
+        }
     }
 
     public static function OrderBy($field) {
-        self::$modelQueryBuilder .= " ORDER BY $field";
-        return new self();
+        try {
+            self::$modelQueryBuilder .= " ORDER BY $field";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("OrderBy Exception: " . $e->getMessage());
+        }
     }
 
     public static function OrderByAsc($field) {
-        self::$modelQueryBuilder .= " ORDER BY $field ASC";
-        return new self();
+        try {
+            self::$modelQueryBuilder .= " ORDER BY $field ASC";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("OrderByAsc Exception: " . $e->getMessage());
+        }
     }
 
     public static function OrderByDesc($field) {
-        self::$modelQueryBuilder .= " ORDER BY $field DESC";
-        return new self();
+        try {
+            self::$modelQueryBuilder .= " ORDER BY $field DESC";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("OrderByDesc Exception: " . $e->getMessage());
+        }
     }
 
     public static function Where($q) {
-        self::$modelQueryBuilder .= " WHERE $q ";
-        return new self();
+        try {
+            self::$modelQueryBuilder .= " WHERE $q ";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("Where Exception: " . $e->getMessage());
+        }
     }
 
     public static function Select($q) {
-        $callingClass = get_called_class();
-        self::$modelQueryBuilder .= "SELECT $q FROM $callingClass";
-        return new self();
+        try {
+            $callingClass = get_called_class();
+            self::$modelQueryBuilder .= "SELECT $q FROM $callingClass";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("Select Exception: " . $e->getMessage());
+        }
     }
 
     public static function First() {
-        self::$modelQueryBuilder .= " LIMIT 1 ";
-        return new self();
+        try {
+            self::$modelQueryBuilder .= " LIMIT 1 ";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("First Exception: " . $e->getMessage());
+        }
     }
 
     public static function Limit($n) {
-        self::$modelQueryBuilder .= " LIMIT $n ";
-        return new self();
+        try {
+            self::$modelQueryBuilder .= " LIMIT $n ";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("Limit Exception: " . $e->getMessage());
+        }
     }
 
     public static function And($q) {
-        self::$modelQueryBuilder .= " AND $q ";
-        return new self();
+        try {
+            self::$modelQueryBuilder .= " AND $q ";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("And Exception: " . $e->getMessage());
+        }
     }
 
     public static function Or($q) {
-        self::$modelQueryBuilder .= " OR $q ";
-        return new self();
+        try {
+            self::$modelQueryBuilder .= " OR $q ";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("Or Exception: " . $e->getMessage());
+        }
     }
 
     public static function To($q) {
-        self::$modelQueryBuilder .= " = '$q' ";
-        return new self();
+        try {
+            self::$modelQueryBuilder .= " = '$q' ";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("To Exception: " . $e->getMessage());
+        }
     }
 
     public static function Is($q) {
-        self::$modelQueryBuilder .= " = '$q' ";
-        return new self();
+        try {
+            self::$modelQueryBuilder .= " = '$q' ";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("Is Exception: " . $e->getMessage());
+        }
     }
 
     public static function Update() {
-        $callingClass = get_called_class();
-        self::$modelQueryBuilder .= "UPDATE $callingClass ";
-        return new self();
+        try {
+            $callingClass = get_called_class();
+            self::$modelQueryBuilder .= "UPDATE $callingClass ";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("Update Exception: " . $e->getMessage());
+        }
     }
 
     public static function Set($q) {
-        self::$modelQueryBuilder .= "SET $q ";
-        return new self();
+        try {
+            self::$modelQueryBuilder .= "SET $q ";
+            return new self();
+        }
+        catch(Exception $e) {
+            throw new Exception("Set Exception: " . $e->getMessage());
+        }
     }
 
     /*public static function Delete() {
@@ -359,27 +481,34 @@ class FluxModel {
         catch (Exception $e) {
             throw new Exception("Error Processing Request", $e->getCode(), $e);
         }
-    }*/
+    } */
 
     public static function Delete($value) {
-        $callingClass = get_called_class();
-        self::$modelQueryBuilder = "DELETE FROM $callingClass WHERE ";
-        $properties = get_class_vars($callingClass);
-        $first = true;
-        foreach ($properties as $propertyName => $propertyValue) {
-            if ($propertyName === 'modelQueryBuilder' || $propertyName === 'modelQueryParams') {
-                continue;
+        try {
+            $callingClass = get_called_class();
+            self::$modelQueryBuilder = "DELETE FROM $callingClass WHERE ";
+            $properties = get_class_vars($callingClass);
+            $first = true;
+            foreach ($properties as $propertyName => $propertyValue) {
+                if ($propertyName === 'modelQueryBuilder' || $propertyName === 'modelQueryParams') {
+                    continue;
+                }
+                if (!$first) {
+                    self::$modelQueryBuilder .= " OR ";
+                } else {
+                    $first = false;
+                }
+                self::$modelQueryBuilder .= "$propertyName = $value";
+                self::$modelQueryParams[] = $value;
             }
-            if (!$first) {
-                self::$modelQueryBuilder .= " OR ";
-            } else {
-                $first = false;
-            }
-            self::$modelQueryBuilder .= "$propertyName = $value";
-            self::$modelQueryParams[] = $value;
+            return new self();
         }
-        return new self();
+        catch(Exception $e) {
+            throw new Exception("Delete Exception: " . $e->getMessage());
+        }
+
     }
+    
 
     public static function Create($obj, $cb = false) {
         try {
@@ -396,18 +525,23 @@ class FluxModel {
             }
             return true;
         } catch (Exception $e) {
-            return false;
+            throw new Exception("Create Exception: " . $e->getMessage());
         }
     }
 
     public static function Exec() {
-        $db = new Flux();
-        $stmt = $db->Query(self::$modelQueryBuilder);
-        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        if (count($data) == 1) {
-            return (object) $data[0];
+        try {
+            $db = new Flux();
+            $stmt = $db->Query(self::$modelQueryBuilder);
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if (count($data) == 1) {
+                return (object) $data[0];
+            }
+            return self::MapData($data);
         }
-        return self::MapData($data);
+        catch (Exception $e) {
+            throw new Exception("Exec Exception: " . $e->getMessage());
+        }
     }
 
     public static function DBQuery() {
