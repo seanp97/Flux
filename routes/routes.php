4b4e14f0fb31a679f45ea2f80c84abd8f891dcdc
Sean Pelser <sean.pelser97@gmail.com>
2024-10-08 15:10:16 +0100

    routes updated

--- routes/routes.php
+++ routes/routes.php
@@ -1,11 +1,37 @@
 <?php
 require './autoload.php';
 
-
 Http::get('/', function() {
     echo 'Hello World';
 });
 
 Http::get('/{id}', function($id) {
     echo 'The ID: ' . $id;
+});
+
+Http::post('/add-user', function() {
+    UserController::AddNewUser();
+});
+
+Http::get('/about', function() {
+    $all_users = AboutController::GetUserAbout();
+    AboutController::index($all_users);
+});
+
+Http::post("/create-user", function() {
+    // Create Person
+    UserController::CreateUser();
+});
+
+Http::post("/edit-user", function() {
+    // Create Person
+    UserController::EditUser();
+});
+
+Http::get('/update-table', function() {
+    // Create User table
+
+    User::MigrateTable(function() {
+        echo Success('User table updated');
+    });
 });
