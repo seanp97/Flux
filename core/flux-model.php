a2480f47a99ced1aab9a63646269ddc53a7c3b82
Sean Pelser <sean.pelser97@gmail.com>
2025-04-22 17:49:29 +0100

    Update flux-model.php - Add method

--- core/flux-model.php
+++ core/flux-model.php
@@ -16,6 +16,35 @@ class FluxModel {
         return $mappedData;
     }
 
+    public static function Add($object, $cb = false) {
+        try {
+            $className = get_called_class();
+            $propertyNames = [];
+            $propertyValues = [];
+            foreach ($object as $propertyName => $propertyValue) {
+                $propertyNames[] = $propertyName;
+                $propertyValues[] = $propertyValue;
+            }
+
+            $columnNames = implode(', ', $propertyNames);
+            $placeholders = rtrim(str_repeat('?, ', count($propertyValues)), ', ');
+
+            $query = "INSERT INTO `$className` ($columnNames) VALUES ($placeholders)";
+
+            $db = new Flux();
+            $stmt = $db->pdo->prepare($query);
+            $stmt->execute((array)$propertyValues);
+
+            if ($cb) {
+                $cb();
+            }
+        } catch (PDOException $e) {
+            echo "PDOException: " . $e->getMessage();
+        } catch (Exception $e) {
+            echo "Exception: " . $e->getMessage();
+        }
+    }
+
     public static function InsertObject($object, $tableName, $cb = false) {
         try {
             $propertyNames = [];
